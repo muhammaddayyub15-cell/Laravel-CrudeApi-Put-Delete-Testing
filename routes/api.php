bfdb8429da02fde74ff8fcb6f0cab9c0b90c39d9
muhammaddayyub15-cell <muhammaddayyub15@gmail.com>
2026-03-17 23:20:46 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,6 @@ use App\Http\Controllers\ProductController;
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::patch('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
